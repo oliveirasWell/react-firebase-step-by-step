629550ed1e86bf068c57e48610fe4014de45f019
Wellington Oliveira <well.oliveira.snt@gmail.com>
2018-03-03 17:39:19 -0400

    Added Material-ui

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import {MuiThemeProvider} from "material-ui/styles/index";
+import {AppBar, IconButton, Toolbar, Typography} from "material-ui";
+import MenuIcon from 'material-ui-icons/Menu';
+import {createMuiTheme} from 'material-ui/styles';
+import red from 'material-ui/colors/red';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: red,
+    },
+});
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          Test
-      </div>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider theme={theme}>
+                <React.Fragment>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <Typography type="title" color="inherit">
+                                My Awesome React App
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                </React.Fragment>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
