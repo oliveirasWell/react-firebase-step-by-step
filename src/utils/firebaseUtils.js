a86ba008fd75233e69b4af8fcf8b2f8dc003dcbb
Wellington Oliveira <well.oliveira.snt@gmail.com>
2018-03-04 03:18:15 -0400

    Tests and select env

--- src/utils/firebaseUtils.js
+++ src/utils/firebaseUtils.js
@@ -1,13 +1,26 @@
 import firebase from 'firebase';
 
-const config = {
-    apiKey: "**************************",
-    authDomain: "**************************",
-    databaseURL: "**************************",
-    projectId: "**************************",
-    storageBucket: "**************************",
-    messagingSenderId: "**************************"
+const prodConfig = {
+    apiKey: "***************",
+    authDomain: "***************",
+    databaseURL: "***************",
+    projectId: "***************",
+    storageBucket: "***************",
+    messagingSenderId: "***************"
 };
+
+const devConfig = {
+    apiKey: "***************",
+    authDomain: "***************",
+    databaseURL: "***************",
+    projectId: "***************",
+    storageBucket: "***************",
+    messagingSenderId: "***************"
+};
+
+const config = process.env.NODE_ENV === 'production'
+    ? prodConfig
+    : devConfig;
 
 export const firebaseImpl = firebase.initializeApp(config);
 export const firebaseDatabase = firebase.database();
